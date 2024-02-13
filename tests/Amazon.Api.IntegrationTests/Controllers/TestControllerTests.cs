947b9ead1de518e6d6fe9124f6ef58238947ca8b
mostafa mohamed <mostafafarag1233@gmail.com>
2024-02-13 15:57:12 +0200

    merge

---
+++ tests/Amazon.Api.IntegrationTests/Controllers/TestControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Api.Controllers;
+using Xunit;
+
+namespace Amazon.Api.IntegrationTests.Controllers
+{
+    public class TestControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsOkResult_WithExpectedMessage()
+        {
+            // Arrange
+            var controller = new TestController();
+
+            // Act
+            var result = controller.Get() as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal(" hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", result.Value);
+        }
+    }
+}
