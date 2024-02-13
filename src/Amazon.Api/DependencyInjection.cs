14a5410278f3fdfcad3ef78de0c43a38fdd69807
Mohammed Hamada <m7amd7amada@gmail.com>
2024-02-13 15:15:52 +0200

    Setup The Project

---
+++ src/Amazon.Api/DependencyInjection.cs
@@ -0,0 +1,13 @@
+namespace Amazon.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddControllers();
+
+        return services;
+    }
+}
